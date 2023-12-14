462b243c7fdfc046e8715c36f768690727d214d0
Nicole Laufer <112592420+nicolelaufer@users.noreply.github.com>
2023-12-14 00:47:16 -0300

    Update volunteerOpportunities.tsx
    Updated data

--- app/data/volunteerOpportunities.tsx
+++ app/data/volunteerOpportunities.tsx
@@ -22,5 +22,53 @@ export const volunteerOpportunities = [
       title: "Psychosocial Support for Conflict-Affected Individuals",
       description: "Provide emotional support to people impacted by difficult situations.",
       location: "Haifa, Israel",
+    },
+    {
+      imageUrl:"",
+      title:"Emergency Medical Aid Team",
+      description:"Join a team providing urgent medical assistance in conflict zones.",
+      location: "Jerusalem,Israel",
+    },
+    {
+      imageUrl:"",
+      title:"Humanitarian Aid Distribution",
+      description:"Assist in distributing essential humanitarian aid to affected communities",
+      location:"Tel Aviv, Israel",
+    },
+    {
+      imageUrl:"",
+      title:"Trauma Counseling for War Survivors",
+      description:"Provide counseling support for individuals affected by war-related trauma.",
+      location: "Haifa,Israel",
+    },
+    {
+      imageUrl:"",
+      title:"Children's Education Support",
+      description:"Support educational initiatives for children affected by conflict.",
+      location:"Beersheba, Israel",
+    },
+    {
+      imageUrl:"",
+      title:"Peace Advocacy Campaign",
+      description:"Participate in campaigns promoting peace and dialogue.",
+      location:"Netanya, Israel",
+    },
+    {
+      imageUrl:"",
+      title:"Community Rebuilding Projects",
+      description:"Contribute to rebuilding efforts in war-affected communities.",
+      location:"Ashkelon, Israel",
+    },
+    {
+      imageUrl:"",
+      title:"Support for Displaced Families",
+      description: "Provide assistance and support to families displaced by conflict.",
+      location:"Beersheba, Israel",
+    },
+    {
+      imageUrl:"",
+      title:"Support for Elderly Citizens",
+      description:"Offer assistance and companionship to elderly citizens.",
+      location:"Petach Tikva, Israel",
     }
   ];
