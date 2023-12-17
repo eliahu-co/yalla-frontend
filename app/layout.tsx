122a98870a6b1232c98ccc89a519dd376b662c48
Nicole Laufer <112592420+nicolelaufer@users.noreply.github.com>
2023-12-17 04:40:38 -0300

    Update layout.tsx
    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { ToasterProvider } from "./providers/ToasterProvider";
 import EventModal from "./Comp/Modals/EventModal";
 import EventCard from "./Comp/EventCard";
 import { volunteerOpportunities } from "./data/volunteerOpportunities";
+import Link from "next/link";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -30,11 +31,15 @@ export default function RootLayout({
         <RegisterModal />
         <LoginModal />
         <Navbar />
-        {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="container mx-auto mt-2 p-4">
+         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-60">
           {volunteerOpportunities.map((event) => (
+            <Link href={`/events/${index}`} key={index}>
             <EventCard key={event.title} {...event}/>
+            </Link>
           ))}
-        </div> */}
+        </div>
+        </div>
         {children}
       </body>
     </html>
