a7a38fee45c92ef39d2cf06fb257e41aff8c5abf
Nicole Laufer <112592420+nicolelaufer@users.noreply.github.com>
2023-12-18 13:14:13 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     <div className="container mx-auto mt-2 p-4">
          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-20">
           {volunteerOpportunities.map((opportunity, index) => (
-            <Link href={`/events/${index}`} key={index}>
+            <Link href={`/events/${encodeURIComponent(opportunity.title)}`} key={index} passHref>
             <EventCard {...opportunity}/>
             </Link>
           ))}
