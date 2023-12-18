7bea7eb1cd3db9fe0c25c17fda3e1e741bb06039
nicolelaufer <nicolelaufer00@gmail.com>
2023-12-18 17:11:43 -0300

    Update EventCard.tsx
    updated

--- app/Comp/EventCard.tsx
+++ app/Comp/EventCard.tsx
@@ -15,29 +15,27 @@ const EventCard: React.FC<EventCardProps> = ({
     description,
     location,
 }) => {
+    const truncateText = (text: string, maxLength: number) => {
+        return text.length > maxLength ? text.substring(0, maxLength) + '...' : text;
+    };
+
     return (
         <div className="
-        border
-        rounded-lg
-        overflow-hidden
-        shadow-md
-        hover:shadow-lg
-        transition
-        cursor-pointer
-        h-full"
-        >
-            <div className="relative h-56 w-full">
-                <Image className="object-cover w-full h-full"
+        max-w-xs mx-auto bg-white shadow-lg rounded-lg overflow-hidden h-[200px] flex flex-col">
+            <div className="aspect-w-16 aspect-h-9">
+                <Image className= "object-cover w-full h-full max-h-40"
                 src={imageUrl}
                 alt={title}
-                layout="fill"/>
+                />
             </div>
-            <div className="p-4">
-                <h2 className="text-xl font-semibold mb-2">{title}</h2>
-                <p className="text-gray-600 mb-2">{description}</p>
-                <p className="text-gray-500">{location}</p>
+            <div className="p-6 flex-grow">
+                <div className="text-xl font-semibold mb-2">{truncateText(title, 20)}</div>
+                <p className="text-gray-600 mb-2">{truncateText(description, 100)}</p>
+                </div>
+                <div className="text-gray-500">
+                    {location}
+                </div>
             </div>
-        </div>
-    )
-}
+    );
+};
 export default EventCard;
