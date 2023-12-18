c2196e7433458b1db64ae9a84b5976fed4fed823
Nicole Laufer <112592420+nicolelaufer@users.noreply.github.com>
2023-12-18 13:12:46 -0300

    Update EventPage.tsx

--- app/EventPage.tsx
+++ app/EventPage.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { volunteerOpportunities } from "./data/volunteerOpportunities";
 import EventCard from "./Comp/EventCard";
+import { useRouter } from "next/router";
 
 
 
-const eventPage: React.FC<EventPageProps> = ({ eventData }) => {
+const EventPage: React.FC = () => {
+    const router = useRouter();
+    const {title} = router.query;
+
+    const eventData = volunteerOpportunities.find((opportunity) => encodeURIComponent(opportunity.title) === title);
+    if (!eventData) {
+        return <div>Event not found</div>
+    }
     return (
         <div>
             <EventCard {...eventData} />
@@ -17,4 +25,4 @@ const eventPage: React.FC<EventPageProps> = ({ eventData }) => {
     )
 };
 
-export default eventPage;
+export default EventPage;
